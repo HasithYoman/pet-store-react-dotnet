c983ef78582464000e50999cdb164a4901be68d2
Hasith Yoman <hasithyoman2@gmail.com>
2023-10-01 16:20:23 +0530

    Backend project withe database connections

---
+++ PetStoreBackEnd/PetStoreBackEnd/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetStoreBackEnd.Entities;
+
+namespace PetStoreBackEnd.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions <ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ProductEntity> Products { get; set; }
+    }
+}
